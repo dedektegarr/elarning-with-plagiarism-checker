0da6a8f26e9242ecc8f7c8b9b6ab10ea34bb738e
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2024-08-06 09:06:12 +0700

    feat: assignment upload loading indicator

--- resources/views/pages/Assignment/show.blade.php
+++ resources/views/pages/Assignment/show.blade.php
@@ -30,9 +30,11 @@
                                 {{ $student->username }}
                             </p>
                         </div>
-                        <div class="inline-flex items-center text-sm font-medium text-gray-900 dark:text-green-600">
-                            Turned in
-                        </div>
+                        <span
+                            class="inline-flex items-center bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">
+                            <span class="w-2 h-2 me-1 bg-green-500 rounded-full"></span>
+                            Turned In
+                        </span>
                     </div>
                 </a>
             </li>
