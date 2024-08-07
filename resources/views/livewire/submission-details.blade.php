d27dbd24210c565ac35fd03ad10e67fec724e65f
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2024-08-07 22:29:02 +0700

    fix: fix some bugs

--- resources/views/livewire/submission-details.blade.php
+++ resources/views/livewire/submission-details.blade.php
@@ -136,8 +136,12 @@
                     </li>
                 </ul>
 
-                <a href="#test"
-                    class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Yellow</a>
+                <form action="">
+                    <button type="button"
+                        class="block w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 
+                        dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Cek
+                        Plagiasi</button>
+                </form>
             </div>
         </div>
     </section>
